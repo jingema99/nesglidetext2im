5daa54545b28f0a11b3e40839418f8eaee221e34
jingema99 <96966394+jingema99@users.noreply.github.com>
2022-09-30 22:22:20 -0400

    Update nes.py

--- glide_text2im/nes.py
+++ glide_text2im/nes.py
@@ -2,6 +2,7 @@
 import torch as th
 import torch.nn as nn
 from glide_text2im.xf import LayerNorm, Transformer, convert_module_to_f16
+from slot_attention import SlotAttention
 
 class NES(nn.Module):
 
@@ -18,6 +19,7 @@ class NES(nn.Module):
 
     self.xf_padding = xf_padding
     self.xf_width = xf_width
+    self.num_events = 1
     
     if xf_final_ln:
          self.final_ln = LayerNorm(xf_width)
@@ -37,6 +39,12 @@ class NES(nn.Module):
         self.padding_embedding = nn.Parameter(
             th.empty(text_ctx, xf_width, dtype=th.float32)
         )
+    self.slot_attn = SlotAttention(
+            num_slots = self.num_events,
+            dim = xf_width,
+            iters = 3
+        )
+
 
 
 
@@ -48,16 +56,15 @@ class NES(nn.Module):
           assert mask is not None
           xf_in = th.where(mask[..., None], xf_in, self.padding_embedding[None])
       #xf_in: [6,8,512]
-      xf_out = self.transformer(xf_in)
-      if self.final_ln is not None:
-          xf_out = self.final_ln(xf_out)
+      xf_out = self.slot_attn(xf_in)
       
-
       Outputs = []
-      xf_proj = self.transformer_proj(xf_out[:, -1])
-      xf_out = xf_out.permute(0, 2, 1)  # NLC -> NCL
-      outputs = dict(xf_proj=xf_proj, xf_out=xf_out[:,:,-1].unsqueeze(2))
-      Outputs.append(outputs)
+      for i in range(self.num_events):
+
+        embedding = xf_out[:, i]
+        xf_proj = self.transformer_proj(embedding)
+        outputs = dict(xf_proj=xf_proj, xf_out=embedding.unsqueeze(2))
+        Outputs.append(outputs)
 
       return Outputs
 #nes.py
