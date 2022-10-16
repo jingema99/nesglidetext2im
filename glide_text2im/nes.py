b2f2ae3c4e7c125f7ef6bf4c97322ab0ef6286f2
jingema99 <96966394+jingema99@users.noreply.github.com>
2022-10-16 11:08:48 -0400

    Update nes.py

--- glide_text2im/nes.py
+++ glide_text2im/nes.py
@@ -3,7 +3,6 @@ import torch as th
 import torch.nn as nn
 from glide_text2im.xf import LayerNorm, Transformer, convert_module_to_f16
 from slot_attention import SlotAttention
-from positional_encodings.torch_encodings import PositionalEncoding1D
 
 class NES(nn.Module):
 
@@ -21,15 +20,21 @@ class NES(nn.Module):
     self.xf_padding = xf_padding
     self.xf_width = xf_width
     self.num_events = 3
+    self.win_size = 4
     
     if xf_final_ln:
          self.final_ln = LayerNorm(xf_width)
 
     if xf_width: 
+        # self.transformer = Transformer(
+        #     text_ctx,
+        #     xf_width,
+        #     xf_layers,
+        #     xf_heads,
+        # )
         self.token_embedding = nn.Embedding(n_vocab, xf_width)
         self.positional_embedding = nn.Parameter(th.empty(text_ctx, xf_width, dtype=th.float32))
         self.transformer_proj = nn.Linear(xf_width, model_channels * 4)
-        self.p_enc = PositionalEncoding1D(xf_width)
 
     if self.xf_padding:
         self.padding_embedding = nn.Parameter(
@@ -41,21 +46,88 @@ class NES(nn.Module):
             iters = 3
         )
 
+    device = th.device("cuda:0")
+    self.w_transformer1 = Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device)                        
+    self.sw_transformer1 = [Transformer(self.win_size/2,xf_width,xf_layers,xf_heads).to(device),
+                           Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device)]
+    self.w_transformer2 = Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device)                        
+    self.sw_transformer2 = [Transformer(self.win_size/2,xf_width,xf_layers,xf_heads).to(device),
+                           Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device)]
+    # self.sw_transformer = [Transformer(self.win_size/2,xf_width,xf_layers,xf_heads).to(device),
+    #                        Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device),
+    #                        Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device),
+    #                        Transformer(self.win_size/2,xf_width,xf_layers,xf_heads).to(device),]
+
+    self.out_transformer = Transformer(self.win_size,xf_width,xf_layers,xf_heads).to(device)
+
+
+  def win_transformer(self, xf_in, size, transformer):
+    
+    num_windows = int(xf_in.shape[1]/size)
+    xf_out = xf_in.new_zeros(xf_in.shape)
+    start = 0
+    for i in range(num_windows):
+      step = size
+      xf_window = xf_in[:,start:start+step,:]
+      xf_out[:,start:start+step,:] = transformer(xf_window)
+      start += step
+      
+    return xf_out
+  
+  def swin_transformer(self, xf_in, size, transformer):
+
+    num_windows = int(xf_in.shape[1]/size) + 1
+    xf_out = xf_in.new_zeros(xf_in.shape)
+    
+    start = 0
+    for i in range(num_windows):
+      if i == 0 or i == num_windows-1:
+        step = int(size/2)
+      else:
+        step = size
+      xf_window = xf_in[:,start:start+step,:]
+      if  i == 0 or i == num_windows-1:
+        xf_out[:,start:start+step,:] = transformer[0](xf_window)
+      else:
+        xf_out[:,start:start+step,:] = transformer[1](xf_window)
+      start += step
+
+    return xf_out
+
+  def out(self, xf_in, size, transformer):
+    
+    num_windows = int(xf_in.shape[1]/size)
+    xf_outs = []
+
+    start = 0
+    for i in range(num_windows):
+      step = size
+      xf_window = xf_in[:,start:start+step,:]
+      xf_outs.append(transformer(xf_window)[:,-1])
+      start += step
+      
+    return xf_outs
+
 
   def forward(self, tokens=None, mask=None):
       assert tokens is not None
-      xf_in = self.token_embedding(tokens.long())
 
-      pos_enc = self.p_enc(xf_in)
-      xf_in = xf_in + pos_enc
+      xf_in = self.token_embedding(tokens.long())
       if self.xf_padding:
           assert mask is not None
           xf_in = th.where(mask[..., None], xf_in, self.padding_embedding[None])
-      #xf_in: [6,8,512]
-      xf_out = self.slot_attn(xf_in)
+
+      #xf_in [B, 12, 9]
+      xf_in = self.win_transformer(xf_in, self.win_size, self.w_transformer1)
+      xf_in = self.swin_transformer(xf_in, self.win_size, self.sw_transformer1)
+      xf_in = self.win_transformer(xf_in, self.win_size, self.w_transformer2)
+      xf_in = self.swin_transformer(xf_in, self.win_size, self.sw_transformer2)
+
+      xf_outs = self.out(xf_in, self.win_size, self.out_transformer)
+
       Outputs = []
-      for i in range(self.num_events):
-        embedding = xf_out[:, i]
+      for i in range(len(xf_outs)):
+        embedding = xf_outs[i]
         xf_proj = self.transformer_proj(embedding)
         outputs = dict(xf_proj=xf_proj, xf_out=embedding.unsqueeze(2))
         Outputs.append(outputs)
